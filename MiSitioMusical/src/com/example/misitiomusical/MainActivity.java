ad054132a748a9109a1a891b7b1eae9c7f92af10
Asus <Asus@Asus-PC>
2013-10-08 23:47:44 -0500

    primera tarea

--- MiSitioMusical/src/com/example/misitiomusical/MainActivity.java
+++ MiSitioMusical/src/com/example/misitiomusical/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.misitiomusical;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -19,5 +21,10 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    public void lanzar(View view) {
+        Intent i = new Intent(this, reg1.class );
+        startActivity(i);
+  }    
     
 }
